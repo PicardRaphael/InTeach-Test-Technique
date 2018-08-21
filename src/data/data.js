e6f097c7f979629890af2855f65eb75096b613cf
Raphael PICARD <raphaelpicard@outlook.fr>
2018-08-21 07:27:55 +0200

    N°1 : Initialisation du projet avec ces divers composants (Vide) et un fichier data

---
+++ src/data/data.js
@@ -0,0 +1,138 @@
+//
+export default [
+  {
+    id: 1,
+    name: 'Révision',
+    by: 'Raphael PICARD',
+    date: '20/08/2018',
+    lessons: [
+      {
+        id: 1,
+        name: 'React',
+        types: 'classique',
+        content: 'React (aussi appelé React.js ou ReactJS) est une bibliothèque JavaScript libre développée par Facebook depuis 2013. Le but principal de cette bibliothèque est de faciliter la création d\'application web monopage, via la création de composants dépendant d\'un état et générant une page (ou portion) HTML à chaque changement d\'état.',
+        url: '',
+        by: 'Raphael PICARD',
+        date: '20/08/2018',
+        cards: [
+          {
+            id: 1,
+            name: 'JSX',
+            content: 'Lorsque l\'on crée des composants React, on utilise du JSX pour définir nos vues. Il s\'agit d\'un sucre syntaxique permettant de décrire la création de nos éléments plus facilement.',
+            url: '',
+            by: 'Raphael PICARD',
+            date: '20/08/2018'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Cours',
+    by: 'Raphael PICARD',
+    date: '20/08/2018',
+    lessons: [
+      {
+        id: 1,
+        name: 'JavaScript',
+        types: 'classique',
+        content: 'JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives mais aussi pour les serveurs2 avec l\'utilisation (par exemple) de Node.js3. C\'est un langage orienté objet à prototype, c\'est-à-dire que les bases du langage et ses principales interfaces sont fournies par des objets qui ne sont pas des instances de classes, mais qui sont chacun équipés de constructeurs permettant de créer leurs propriétés, et notamment une propriété de prototypage qui permet d\'en créer des objets héritiers personnalisés.',
+        url: '',
+        by: 'Raphael PICARD',
+        date: '20/08/2018',
+        cards: [
+          {
+            id: 1,
+            name: 'Programmation fonctionnelle',
+            content: 'JavaScript permet à de nombreux égards d\'utiliser les paradigmes de la programmation fonctionnelle. Il s\'agit avant tout de multiplier le recours aux fonctions pour mieux compartimenter son code.',
+            url: '',
+            by: 'Raphael PICARD',
+            date: '20/08/2018'
+          },
+          {
+            id: 2,
+            name: 'Les fonctions',
+            content: 'Des fonctions comme alert() sont disponible par défaut dans JavaScript. Mais on peut aussi définir nous-même nos propres fonctions. Ça va nous permettre d\'exécuter une série d\'instructions. On va pouvoir lui donner un ou des paramètres qui seront repris dans le corps de la fonction.',
+            url: '',
+            by: 'Raphael PICARD',
+            date: '20/08/2018'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Jeux',
+    by: 'Raphael PICARD',
+    date: '20/08/2018',
+    lessons: [
+      {
+        id: 1,
+        name: 'Javascript',
+        types: 'évalutation',
+        content: 'Réaliser le niveau 1 et 2 sur JavaScript',
+        url: 'https://codecombat.com/',
+        by: 'Raphael PICARD',
+        date: '20/08/2018',
+        cards: []
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Vidéos',
+    by: 'Raphael PICARD',
+    date: '20/08/2018',
+    lessons: [
+      {
+        id: 1,
+        name: 'React',
+        types: 'classique',
+        content: 'Pourquoi utiliser React.js ?',
+        url: 'https://www.youtube.com/watch?v=CV-6yLgszX0',
+        by: 'Raphael PICARD',
+        date: '20/08/2018',
+        cards: [
+          {
+            id: 1,
+            name: 'Reactjs Tout sur le JSX',
+            content: 'Anthony Welc te montre la puissance du JSX en React.js pour développer rapidement et plus facilement des applications web.',
+            url: 'https://www.youtube.com/watch?v=hMlUh3NXI4c&list=PLHSUbP5y6J0UeOI0akEWvSdJNwbrYAc6L',
+            by: 'Raphael PICARD',
+            date: '20/08/2018'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Quiz',
+    by: 'Raphael PICARD',
+    date: '20/08/2018',
+    lessons: [
+      {
+        id: 1,
+        name: 'React',
+        types: 'évaluation',
+        content: 'Ce test permet de vérifier vos compétences sur React.js',
+        url: 'https://jobprod.com/qcm-informatique/qcm-reactjs/',
+        by: 'Raphael PICARD',
+        date: '20/08/2018',
+        cards: []
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+        types: 'évaluation',
+        content: 'Ce test permet de vérifier vos compétences sur JavaScript',
+        url: 'https://jobprod.com/qcm-informatique/qcm-javascript/',
+        by: 'Raphael PICARD',
+        date: '20/08/2018',
+        cards: []
+      }
+    ]
+  }
+];
